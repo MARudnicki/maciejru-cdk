780625e2b61626c4e03a612fab281d5cfe2a7473
Maciej Rudnicki <maciejru@amazon.com>
2021-12-10 21:42:01 +0100

    clean up

--- bin/maciejru-cdk.ts
+++ bin/maciejru-cdk.ts
@@ -2,27 +2,16 @@
 import 'source-map-support/register';
 import {MaciejruCdkStack} from '../lib/maciejru-cdk-stack';
 import {BillingStack} from "../lib/biling-stack";
-import {App} from "@aws-cdk/core";
 import {PipelineStack} from "../lib/pipeline-stack";
+import {App} from "@aws-cdk/core";
 
 const app = new App();
 new PipelineStack(app, 'MaciejruCdkStack', {
-    /* If you don't specify 'env', this stack will be environment-agnostic.
-     * Account/Region-dependent features and context lookups will not work,
-     * but a single synthesized template can be deployed anywhere. */
 
-    /* Uncomment the next line to specialize this stack for the AWS Account
-     * and Region that are implied by the current CLI configuration. */
-    // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-    /* Uncomment the next line if you know exactly what Account and Region you
-     * want to deploy the stack to. */
     env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
         region: process.env.CDK_DEFAULT_REGION
     },
-
-    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
 
 new BillingStack(app, "BillingStack", {
