f51aa2be34700c2669fadfaadde2d3214f0f6a17
Maciej Rudnicki <maciejru@amazon.com>
2021-12-11 10:41:33 +0100

    Fix test

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -1,14 +1,17 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as Pipeline from '../lib/pipeline-stack';
-import {PipelineStack} from "../lib/pipeline-stack";
+import {
+    expect as expectCDK,
+    matchTemplate,
+    MatchStyle,
+    SynthUtils,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as Pipeline from "../lib/pipeline-stack";
 
-test('Empty Stack', () => {
+test("Pipeline Stack", () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new PipelineStack(app, 'MyTestStack', {});
+    const stack = new Pipeline.PipelineStack(app, "MyTestStack", {});
     // THEN
-    expectCDK(stack).to(matchTemplate({
-        "Resources": {}
-    }, MatchStyle.EXACT))
+
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
 });
