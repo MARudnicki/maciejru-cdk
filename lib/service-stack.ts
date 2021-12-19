4963f731522a4906ab281b4c7dae5ffed28727f6
Maciej Rudnicki <maciejru@amazon.com>
2021-12-19 16:06:42 +0100

    Second Api Gateway

--- lib/service-stack.ts
+++ lib/service-stack.ts
@@ -1,28 +1,47 @@
 import {Construct, Stack, StackProps} from "@aws-cdk/core";
 import {CfnParametersCode, Code, Function, Runtime} from "@aws-cdk/aws-lambda";
-import {HttpApi} from "@aws-cdk/aws-apigatewayv2";
-import {LambdaProxyIntegration} from "@aws-cdk/aws-apigatewayv2-integrations";
+import {HttpApi, HttpMethod} from "@aws-cdk/aws-apigatewayv2";
+import {HttpProxyIntegration, LambdaProxyIntegration} from "@aws-cdk/aws-apigatewayv2-integrations";
 
 export class ServiceStack extends Stack {
 
     public readonly serviceCode: CfnParametersCode
+
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
         this.serviceCode = Code.fromCfnParameters()
 
-        const lambda = new Function(this, "ServiceLambda", {
+        const lambda: Function = new Function(this, "ServiceLambda", {
             runtime: Runtime.NODEJS_14_X,
             handler: 'src/lambda.handler',
             code: this.serviceCode,
             functionName: 'ServiceLambda'
         })
 
-        new HttpApi(this, 'ServiceApi', {
+        const innerApiGateway: HttpApi = new HttpApi(this, 'InnerApiGateway', {
             defaultIntegration: new LambdaProxyIntegration({
                 handler: lambda
             }),
-            apiName: 'MaciejruService'
+            apiName: 'InnerApiGateway'
         })
+
+        const outerApiGateway = new HttpApi(this, 'outerApiGateway')
+        outerApiGateway.addRoutes({
+            path: "/",
+            methods: [HttpMethod.GET],
+            integration: new HttpProxyIntegration({
+                url: innerApiGateway.url || "",
+            })
+        })
+
+        outerApiGateway.addRoutes({
+            path: "/test",
+            methods: [HttpMethod.GET],
+            integration: new HttpProxyIntegration({
+                url: "https://google.com"
+            })
+        })
+
     }
 }
