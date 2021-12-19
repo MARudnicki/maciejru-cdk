b7f68655f41908c156d50c3646d331a0a144f47e
Maciej Rudnicki <maciejru@amazon.com>
2021-12-19 15:19:58 +0100

    pipeline test

---
+++ test/pipeline.test.ts
@@ -0,0 +1,19 @@
+import {App} from "@aws-cdk/core";
+import {ServiceStack} from "../lib/service-stack";
+import {PipelineStack} from "../lib/pipeline-stack";
+import {arrayWith, haveResourceLike, objectLike} from "@aws-cdk/assert";
+
+test("Adding service stage", () => {
+    ///Given
+    const app = new App()
+    const serviceStack = new ServiceStack(app, "ServiceStack")
+    const pipelineStack = new PipelineStack(app, "PipelineStack", {})
+
+    //When
+    pipelineStack.addServiceStage(serviceStack, "Test")
+
+    //Then
+    expect(pipelineStack).toBe(
+        haveResourceLike("AWS::CodePipeline::Pipeline")
+    );
+});
