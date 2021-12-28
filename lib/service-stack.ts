0fa81636f80903f735e757f792b85ef40e2f7c2c
Maciej Rudnicki <maciejru@amazon.com>
2021-12-28 10:21:57 +0100

    Add Alarms

--- lib/service-stack.ts
+++ lib/service-stack.ts
@@ -1,8 +1,9 @@
-import {CfnOutput, Construct, Stack, StackProps} from "@aws-cdk/core";
+import {CfnOutput, Construct, Duration, Stack, StackProps} from "@aws-cdk/core";
 import {Alias, CfnParametersCode, Code, Function, Runtime} from "@aws-cdk/aws-lambda";
 import {HttpApi, HttpMethod} from "@aws-cdk/aws-apigatewayv2";
 import {HttpProxyIntegration, LambdaProxyIntegration} from "@aws-cdk/aws-apigatewayv2-integrations";
 import {LambdaDeploymentConfig, LambdaDeploymentGroup} from "@aws-cdk/aws-codedeploy";
+import {TreatMissingData} from "@aws-cdk/aws-cloudwatch";
 
 interface ServiceStackProps extends StackProps {
     stageName : string,
@@ -50,7 +51,23 @@ export class ServiceStack extends Stack {
 
             new LambdaDeploymentGroup(this, 'DeploymentGroup', {
                 alias:alias,
-                deploymentConfig: LambdaDeploymentConfig.CANARY_10PERCENT_5MINUTES
+                deploymentConfig: LambdaDeploymentConfig.CANARY_10PERCENT_5MINUTES,
+                autoRollback: {
+                    deploymentInAlarm: true
+                },
+                alarms: [
+                    innerApiGateway.metricServerError()
+                        .with({
+                            period: Duration.minutes(1)
+                        })
+                        .createAlarm(this, 'ServiceErrorAlarm', {
+                        threshold: 1,
+                        alarmDescription: "Service experienced error",
+                        alarmName: `My severe alarm ${props.stageName}`,
+                        evaluationPeriods: 1,
+                        treatMissingData: TreatMissingData.NOT_BREACHING
+                    })
+                ]
             })
 
         }
